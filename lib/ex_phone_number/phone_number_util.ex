c64768868b5e0790a44bc47078f0917b525b6486
josemrb <work@josemrb.com>
2016-03-07 18:11:56 -0400

    update module PhoneNumberUtil + viable_phone_number?
    updates tests PhoneNumberUtilSpec

--- lib/ex_phone_number/phone_number_util.ex
+++ lib/ex_phone_number/phone_number_util.ex
@@ -16,24 +16,24 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   end
 
   defp build_national_number(number_to_parse) do
-    _national_number =
-      case have_phone_context(number_to_parse) do
-        {:true, result} -> parse_rfc3699(number_to_parse, result)
-        :false -> extract_possible_number(number_to_parse)
-      end
+    case have_phone_context(number_to_parse) do
+      {:true, result} -> parse_rfc3699(number_to_parse, result)
+      :false -> extract_possible_number(number_to_parse)
+    end
+    |> delete_isdn_subaddress()
   end
 
   @rfc3966_phone_context ";phone-context="
   defp have_phone_context(number_to_parse) do
-    case :binary.match(number_to_parse, @rfc3966_phone_context, []) do
-      {pos, length} -> {:true, %{pos: pos, length: length}}
+    case :binary.match(number_to_parse, @rfc3966_phone_context) do
+      {index, length} -> {:true, %{index: index, length: length}}
       :nomatch -> :false
     end
   end
 
   @plus_sign '+'
-  defp parse_rfc3699(number_to_parse, %{pos: pos, length: length}) do
-    {:error, "Not implemented. number: #{number_to_parse} pos: #{pos} length: #{length}"}
+  defp parse_rfc3699(number_to_parse, %{index: index, length: length}) do
+    {:error, "Not implemented. number: #{number_to_parse} index: #{index} length: #{length}"}
   end
 
   @plus_chars "+\uFF0B"
@@ -41,7 +41,7 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   @valid_start_char_pattern ~r/[#{@plus_chars}#{@valid_digits}]/u
   def extract_possible_number(number_to_parse) do
     case Regex.run(@valid_start_char_pattern, number_to_parse, return: :index) do
-      [{index, _match}] ->
+      [{index, _length}] ->
         {_head, tail} = String.split_at(number_to_parse, index)
         tail
         |> remove_unwanted_end_char_pattern()
@@ -53,9 +53,8 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   @valid_alpha "A-Za-z"
   @unwanted_end_char_pattern ~r/[^#{@valid_digits}#{@valid_alpha}#]+$/u
   defp remove_unwanted_end_char_pattern(number_to_parse) do
-    String.replace(number_to_parse, @unwanted_end_char_pattern, "")
     case Regex.run(@unwanted_end_char_pattern, number_to_parse, return: :index) do
-      [{index, _match}] ->
+      [{index, _length}] ->
         {head, _tail} = String.split_at(number_to_parse, index)
         head
       nil -> number_to_parse
@@ -65,10 +64,41 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   @second_number_start_pattern ~r/[\\\/] *x/u
   defp remove_second_number(number_to_parse) do
     case Regex.run(@second_number_start_pattern, number_to_parse, return: :index) do
-      [{index, _match}] ->
+      [{index, _length}] ->
         {head, _tail} = String.split_at(number_to_parse, index)
         head
       nil -> number_to_parse
+    end
+  end
+
+  @rfc3966_isdn_subaddress ";isub="
+  defp delete_isdn_subaddress(number_to_parse) do
+    case :binary.match(number_to_parse, @rfc3966_isdn_subaddress) do
+      {index, _length} ->
+      {head, _tail} = String.split_at(number_to_parse, index)
+      head
+      :nomatch -> number_to_parse
+    end
+  end
+
+  @min_length_for_nsn "2"
+  @min_length_phone_number_pattern  "[#{@valid_digits}]{#{@min_length_for_nsn}}"
+  @valid_punctuation "-x\u2010-\u2015\u2212\u30FC\uFF0D-\uFF0F \u00A0\u00AD\u200B\u2060\u3000()\uFF08\uFF09\uFF3B\uFF3D.\\[\\]/~\u2053\u223C\uFF5E"
+  @star_sign "*"
+  @valid_phone_number "[#{@plus_chars}]*(?:[#{@valid_punctuation}#{@star_sign}]*[#{@valid_digits}]){3,}[#{@valid_punctuation}#{@star_sign}#{@valid_alpha}#{@valid_digits}]*"
+  @rfc3966_extn_prefix ";ext="
+  @capturing_extn_digits "([#{@valid_digits}]{1,7})"
+  @extn_patterns_for_parsing "#{@rfc3966_extn_prefix}#{@capturing_extn_digits}|[ \u00A0\\t,]*(?:e?xt(?:ensi(?:o\u0301?|\u00F3))?n?|\uFF45?\uFF58\uFF54\uFF4E?|[,x\uFF58#\uFF03~\uFF5E]|int|anexo|\uFF49\uFF4E\uFF54)[:\\.\uFF0E]?[ \u00A0\\t,-]*#{@capturing_extn_digits}#?|[- ]+([#{@valid_digits}]{1,5})#"
+  @valid_phone_number_pattern ~r/^#{@min_length_phone_number_pattern}$|^#{@valid_phone_number}(?:#{@extn_patterns_for_parsing})?$/iu
+
+  def viable_phone_number?(phone_number) do
+    String.length(phone_number) < Integer.parse(@min_length_for_nsn) and matches_entirely?(@valid_phone_number_pattern, phone_number)
+  end
+
+  defp matches_entirely?(regex, string) do
+    case Regex.run(regex, string, return: :index) do
+      [{_index, length}] -> String.length(string) == length
+      _ -> false
     end
   end
 end
--- test/ex_phone_number/phone_number_util_test.exs
+++ test/ex_phone_number/phone_number_util_test.exs
@@ -61,4 +61,51 @@ defmodule ExPhoneNumber.PhoneNumberUtilSpec do
       end
     end
   end
+
+  describe ".viable_phone_number?" do
+    context "ascii chars" do
+      it "should contain at least 2 chars" do
+        refute PhoneNumberUtil.viable_phone_number?("1")
+      end
+
+      it "should allow only one or two digits before strange non-possible puntuaction" do
+        refute PhoneNumberUtil.viable_phone_number?("1+1+1")
+        refute PhoneNumberUtil.viable_phone_number?("80+0")
+      end
+
+      it "should allow two or more digits" do
+        assert PhoneNumberUtil.viable_phone_number?("00")
+        assert PhoneNumberUtil.viable_phone_number?("111")
+      end
+
+      it "should allow alpha numbers" do
+        assert PhoneNumberUtil.viable_phone_number?("0800-4-pizza")
+        assert PhoneNumberUtil.viable_phone_number?("0800-4-PIZZA")
+      end
+
+      it "should contain at least three digits before any alpha char" do
+        refute PhoneNumberUtil.viable_phone_number?("08-PIZZA")
+        refute PhoneNumberUtil.viable_phone_number?("8-PIZZA")
+        refute PhoneNumberUtil.viable_phone_number?("12. March")
+      end
+    end
+
+    context "non-ascii chars" do
+      @tag :pending
+      it "should allow only one or two digits before strange non-possible puntuaction" do
+        assert PhoneNumberUtil.viable_phone_number?("1\u300034")
+        refute PhoneNumberUtil.viable_phone_number?("1\u30003+4")
+      end
+
+      @tag :pending
+      it "should allow unicode variants of starting chars" do
+        assert PhoneNumberUtil.viable_phone_number?("\uFF081\uFF09\u30003456789")
+      end
+
+      @tag :pending
+      it "should allow leading plus sign" do
+        assert PhoneNumberUtil.viable_phone_number?("+1\uFF09\u30003456789")
+      end
+    end
+  end
 end
