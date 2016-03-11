00de65ce69a1c15703bf075225d7b4c134de6518
josemrb <work@josemrb.com>
2016-03-11 15:16:34 -0400

    add module ExPhoneNumber.PhoneNumber.NumberFormat
    add module ExPhoneNumber.PhoneNumber.Phone.Metadata
    
    add module ExPhoneNumber.PhoneNumber.PhoneNumberMetadata

---
+++ lib/ex_phone_number/phone_number/phone_number_description.ex
@@ -0,0 +1,5 @@
+defmodule ExPhoneNumber.PhoneNumber.PhoneNumberDescription do
+  defstruct national_number_pattern: nil, # string
+            possible_number_pattern: nil, # string
+            example_number: nil           # string
+end
