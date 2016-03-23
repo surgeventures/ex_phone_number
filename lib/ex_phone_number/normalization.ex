3646dbfeae6ae6acb2dd352c886f951fa22be833
josemrb <work@josemrb.com>
2016-03-23 01:13:19 -0400

    add module ExPhoneNumber.Constant.Mapping
    update module ExPhoneNumber.Normalization
    + convert_alpha_chars_in_number
    + normalize
    + normalize_digits_only
    
    add tests ExPhoneNumber.NormalizationSpec

--- lib/ex_phone_number/normalization.ex
+++ lib/ex_phone_number/normalization.ex
@@ -1,4 +1,8 @@
 defmodule ExPhoneNumber.Normalization do
+  import ExPhoneNumber.Util
+  alias ExPhoneNumber.Constant.Mapping
+  alias ExPhoneNumber.Constant.Pattern
+  alias ExPhoneNumber.Constant.Value
 
   def split_at_match_and_return_head(string, pattern) when is_binary(string) and is_map(pattern) do
     case Regex.run(pattern, string, return: :index) do
@@ -21,4 +25,35 @@ defmodule ExPhoneNumber.Normalization do
   def is_nil_or_empty?(nil), do: true
   def is_nil_or_empty?(string) when is_binary(string), do: String.length(string) == 0
   def is_nil_or_empty?(_), do: false
+
+  def normalize(number) do
+    if matches_entirely?(Pattern.valid_alpha_phone_pattern, number) do
+      normalize_helper(number, Mapping.all_normalization_mappings, true)
+    else
+      normalize_digits_only(number)
+    end
+  end
+
+  def normalize_digits_only(number) do
+    normalize_helper(number, Mapping.digit_mappings, true)
+  end
+
+  def convert_alpha_chars_in_number(number) do
+    normalize_helper(number, Mapping.all_normalization_mappings, false)
+  end
+
+  def normalize_helper(number, normalization_replacements, remove_non_matches) when is_binary(number) and is_map(normalization_replacements) and is_boolean(remove_non_matches) do
+    Enum.reduce(String.codepoints(number), [], fn(char, list) ->
+      if new_char = Map.get(normalization_replacements, String.upcase(char)) do
+        list ++ [new_char]
+      else
+        if remove_non_matches do
+          list
+        else
+          list ++ [char]
+        end
+      end
+    end)
+    |> List.to_string()
+  end
 end
---
+++ test/ex_phone_number/normalization_test.exs
@@ -0,0 +1,60 @@
+defmodule ExPhoneNumber.NormalizationSpec do
+  use Pavlov.Case
+
+  doctest ExPhoneNumber.Normalization
+  import ExPhoneNumber.Normalization
+
+  describe ".convert_alpha_chars_in_number" do
+    context "1-800-ABC" do
+      subject do: "1800-ABC-DEF"
+
+      it "returns correct value" do
+        assert "1800-222-333" == convert_alpha_chars_in_number(subject)
+      end
+    end
+  end
+
+  describe ".normalize" do
+    context "number with puntuaction" do
+      subject do: "034-56&+#2\u00AD34"
+
+      it "returns correct value" do
+        assert "03456234" == normalize(subject)
+      end
+    end
+
+    context "number with alpha chars" do
+      subject do: "034-I-am-HUNGRY"
+
+      it "returns correct value" do
+        assert "034426486479" == normalize(subject)
+      end
+    end
+
+    context "number with unicode digits" do
+      subject do: "\uFF125\u0665"
+
+      it "returns correct value" do
+        assert "255" == normalize(subject)
+      end
+    end
+
+    context "number with eastern-arabic digits" do
+      subject do: "\u06F52\u06F0"
+
+      it "returns correct value" do
+        assert "520" == normalize(subject)
+      end
+    end
+  end
+
+  describe "normalize_digits_only" do
+    context "number with alpha chars and puntuaction" do
+      subject do: "034-56&+a#234"
+
+      it "returns correct value" do
+        assert "03456234" == normalize_digits_only(subject)
+      end
+    end
+  end
+end
