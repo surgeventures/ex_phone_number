d4b82fd10551be7c4bd91a42df8b22743468dc45
josemrb <work@josemrb.com>
2016-03-23 16:55:24 -0400

    update module ExPhoneNumber.Util + is_number_matching_description?

--- lib/ex_phone_number/util.ex
+++ lib/ex_phone_number/util.ex
@@ -1,4 +1,6 @@
 defmodule ExPhoneNumber.Util do
+  alias ExPhoneNumber.Metadata.PhoneNumberDescription
+
   @doc ~S"""
   Returns a boolean indicating whether there was a match and
   is the same size as the `string` parameter.
@@ -9,5 +11,10 @@ defmodule ExPhoneNumber.Util do
       [{_index, length}] -> Kernel.byte_size(string) == length
       _ -> false
     end
+  end
+
+  def is_number_matching_description?(number, %PhoneNumberDescription{} = description) when is_binary(number) do
+    matches_entirely?(description.possible_number_pattern, number) and
+      matches_entirely?(description.national_number_pattern, number)
   end
 end
