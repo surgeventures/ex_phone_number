0e2c96049968155f637f8f7b669610d827da1796
josemrb <work@josemrb.com>
2016-03-23 03:50:45 -0400

    update module ExPhoneNumber.Metadata
    update tests ExPhoneNumber.MetadataSpec

--- lib/ex_phone_number/metadata.ex
+++ lib/ex_phone_number/metadata.ex
@@ -29,27 +29,72 @@ defmodule ExPhoneNumber.Metadata do
     Module.put_attribute(__MODULE__, :list_country_code_to_region_code, {country_code_atom, region})
   end
 
+  def get_for_non_geographical_region(calling_code) when is_number(calling_code) do
+    region_code = get_region_code_for_country_code(calling_code)
+    get_for_region_code(region_code, calling_code)
+  end
+
   def get_for_region_code(region_code) when is_binary(region_code) do
     Keyword.get(list_region_code_to_metadata, String.to_atom(String.upcase(region_code)))
   end
   def get_for_region_code(_), do: nil
 
+  def get_for_region_code(region_code, country_code) when is_binary(region_code) and is_number(country_code) do
+    possible_metadatas = Keyword.get_values(list_region_code_to_metadata, String.to_atom(String.upcase(region_code)))
+    Enum.find(possible_metadatas, fn(metadata) -> country_code == metadata.country_code end)
+  end
+
   def get_region_code_for_country_code(country_code) when is_number(country_code) do
-    Keyword.get(list_country_code_to_region_code, String.to_atom(Integer.to_string(country_code)))
+    #Keyword.get(list_country_code_to_region_code, String.to_atom(Integer.to_string(country_code)))
+    possible_region_codes = get_region_codes_for_country_code(country_code)
+    possible_metadatas = Enum.map(possible_region_codes, fn(region_code) -> get_for_region_code(region_code) end)
+    metadata = Enum.find(possible_metadatas, fn(metadata) -> metadata.main_country_for_code end)
+    if is_nil(metadata) do
+      List.last(possible_metadatas).id
+    else
+      metadata.id
+    end
   end
   def get_region_code_for_country_code(_), do: nil
+
+  def get_region_codes_for_country_code(country_code) when is_number(country_code) do
+    Keyword.get_values(list_country_code_to_region_code, String.to_atom(Integer.to_string(country_code)))
+  end
 
   def list_region_code_to_metadata, do: @list_region_code_to_metadata
 
   def list_country_code_to_region_code, do: @list_country_code_to_region_code
 
   def valid_region_code?(region_code) when is_binary(region_code) do
-    if Integer.parse(region_code) == :error do
+    unless Integer.parse(region_code) == :error do
       false
     else
-      Keyword.has_key?(list_region_code_to_metadata, String.upcase(region_code))
+      Keyword.has_key?(list_region_code_to_metadata, String.to_atom(String.upcase(region_code)))
     end
   end
   def valid_region_code?(_), do: nil
 
+  def get_country_code_for_region_code(region_code) do
+    if valid_region_code?(region_code) do
+      get_for_region_code(region_code).country_code
+    else
+      0
+    end
+  end
+
+  def get_supported_regions() do
+    Enum.filter(list_region_code_to_metadata, fn({region_code, metadata}) -> Integer.parse(metadata.id) == :error end)
+  end
+
+  def is_supported_region?(region_code) when is_binary(region_code) do
+    Enum.any?(get_supported_regions, fn({_, metadata}) -> metadata.id == region_code end)
+  end
+
+  def get_supported_global_network_calling_codes() do
+    Enum.filter(list_region_code_to_metadata, fn({region_code, metadata}) -> Integer.parse(metadata.id) != :error end)
+  end
+
+  def is_supported_global_network_calling_code?(calling_code) when is_number(calling_code) do
+    Enum.any?(get_supported_global_network_calling_codes, fn({_, metadata}) -> metadata.country_code == calling_code end)
+  end
 end
--- test/ex_phone_number/metadata_test.exs
+++ test/ex_phone_number/metadata_test.exs
@@ -178,6 +178,199 @@ defmodule ExPhoneNumber.MetadataSpec do
         assert "$1 $2 $3 $4" == Enum.at(metadata.intl_number_format, 3).format
       end
     end
+  end
 
+  describe ".get_for_non_geographical_region" do
+    context "800 calling code" do
+      subject do: 800
+      let :metadata do
+        ExPhoneNumber.Metadata.get_for_non_geographical_region(subject)
+      end
+
+      it "returns valid id" do
+        assert "001" == metadata.id
+      end
+
+      it "returns valid country_code" do
+        assert 800 == metadata.country_code
+      end
+
+      it "returns valid number_format(0).format" do
+        assert "$1 $2" == Enum.at(metadata.number_format, 0).format
+      end
+
+      it "returns valid number_format(0).pattern" do
+        assert "(\\d{4})(\\d{4})" == Enum.at(metadata.number_format, 0).pattern
+      end
+
+      it "returns valid general.example_number" do
+        assert "12345678" == metadata.general.example_number
+      end
+
+      it "returns valid toll_free.example_number" do
+        assert "12345678" == metadata.toll_free.example_number
+      end
+    end
+  end
+
+  describe ".get_supported_regions/1" do
+    context "get a list of supported regions" do
+      it "should contain at least one element" do
+        assert 0 < length(get_supported_regions)
+      end
+    end
+  end
+
+  describe ".is_supported_region?/1" do
+    context "US" do
+      it "returns true" do
+        assert is_supported_region?("US")
+      end
+    end
+
+    context "001" do
+      it "returns false" do
+        refute is_supported_region?("001")
+      end
+
+      it "returns false" do
+        refute is_supported_region?("800")
+      end
+    end
+  end
+
+  describe ".get_supported_global_network_calling_codes/1" do
+    context "get a list of supported global network calling codes" do
+      it "contains at least one element" do
+        assert 0 < length(get_supported_global_network_calling_codes)
+      end
+    end
+
+    context "test all the calling codes" do
+      it "contains all the calling codes" do
+        assert Enum.all?(get_supported_global_network_calling_codes, fn({_, metadata}) -> metadata.id == "001" end)
+      end
+    end
+  end
+
+  describe ".is_supported_global_network_calling_code?/1" do
+    context "US" do
+      it "returns false" do
+        refute is_supported_global_network_calling_code?(1)
+      end
+    end
+
+    context "800" do
+      it "returns true" do
+        assert is_supported_global_network_calling_code?(800)
+      end
+    end
+  end
+
+  describe ".get_region_code_for_country_code/1" do
+    context "1" do
+      it "returns correct value" do
+        assert "US" == get_region_code_for_country_code(1)
+      end
+    end
+
+    context "44" do
+      it "returns correct value" do
+        assert "GB" == get_region_code_for_country_code(44)
+      end
+    end
+
+    context "49" do
+      it "returns correct value" do
+        assert "DE" == get_region_code_for_country_code(49)
+      end
+    end
+
+    context "800" do
+      it "returns correct value" do
+        assert "001" == get_region_code_for_country_code(800)
+      end
+    end
+
+    context "979" do
+      it "returns correct value" do
+        assert "001" == get_region_code_for_country_code(979)
+      end
+    end
+  end
+
+  describe ".get_region_codes_for_country_code/1" do
+    context "1" do
+      it "should contains US BS" do
+        list = get_region_codes_for_country_code(1)
+        assert Enum.any?(list, &(&1 == "US"))
+        assert Enum.any?(list, &(&1 == "BS"))
+      end
+    end
+
+    context "44" do
+      it "should contains GB" do
+        list = get_region_codes_for_country_code(44)
+        assert Enum.any?(list, &(&1 == "GB"))
+      end
+    end
+
+    context "49" do
+      it "should contains DE" do
+        list = get_region_codes_for_country_code(49)
+        assert Enum.any?(list, &(&1 == "DE"))
+      end
+    end
+
+    context "800" do
+      it "should contains 001" do
+        list = get_region_codes_for_country_code(800)
+        assert Enum.any?(list, &(&1 == "001"))
+      end
+    end
+
+    context "-1" do
+      it "should be empty" do
+        assert 0 == length(get_region_codes_for_country_code(-1))
+      end
+    end
+  end
+
+  describe ".get_country_code_for_region_code/1" do
+    context "US" do
+      it "returns correct value" do
+        assert 1 == get_country_code_for_region_code "US"
+      end
+    end
+
+    context "NZ" do
+      it "returns correct value" do
+        assert 64 == get_country_code_for_region_code "NZ"
+      end
+    end
+
+    context "nil" do
+      it "returns correct value" do
+        assert 0 == get_country_code_for_region_code nil
+      end
+    end
+
+    context "ZZ" do
+      it "returns correct value" do
+        assert 0 == get_country_code_for_region_code "ZZ"
+      end
+    end
+
+    context "001" do
+      it "returns correct value" do
+        assert 0 == get_country_code_for_region_code "001"
+      end
+    end
+
+    context "CS" do
+      it "returns correct value" do
+        assert 0 == get_country_code_for_region_code "CS"
+      end
+    end
   end
 end
