3648e56691c44b210f003b44dce72455e825f2a3
josemrb <work@josemrb.com>
2016-03-23 16:59:11 -0400

    update module ExPhoneNumber.PhoneNumberUtil * parse + parse_helper

--- lib/ex_phone_number/phone_number_util.ex
+++ lib/ex_phone_number/phone_number_util.ex
@@ -5,14 +5,35 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   alias ExPhoneNumber.Constant.ErrorMessage
   alias ExPhoneNumber.Constant.Pattern
   alias ExPhoneNumber.Constant.Value
+  alias ExPhoneNumber.PhoneNumber
 
-  def parse(number_to_parse, _region) when is_nil(number_to_parse), do: {:error, ErrorMessage.not_a_number}
-  def parse(number_to_parse, region) when is_binary(number_to_parse) and is_binary(region) do
-    validate_length(number_to_parse) # {:ok // {:error
-    build_national_number_for_parsing(number_to_parse) # string
-    viable_phone_number?(number_to_parse) # boolean
-    check_region_for_parsing(number_to_parse, region) # boolean
-    maybe_strip_extension(number_to_parse) # string
+  def parse(number_to_parse, default_region) do
+    parse_helper(number_to_parse, default_region, false, true)
+  end
+
+  def parse_helper(number_to_parse, _default_region, _keep_raw_input, _check_region) when is_nil(number_to_parse), do: {:error, ErrorMessage.not_a_number}
+  def parse_helper(number_to_parse, default_region, keep_raw_input, check_region) when is_binary(number_to_parse) and is_binary(default_region) and is_boolean(keep_raw_input) and is_boolean(check_region) do
+    case validate_length(number_to_parse) do
+      {:error, message} -> {:error, message}
+      {:ok, number_to_parse} ->
+        national_number = build_national_number_for_parsing(number_to_parse)
+        unless is_viable_phone_number?(national_number) do
+          {:error, ErrorMessage.not_a_number}
+        else
+          if check_region and not check_region_for_parsing(national_number, default_region) do
+            {:error, ErrorMessage.invalid_country_code}
+          else
+            phone_number = if keep_raw_input, do: %PhoneNumber{raw_input: number_to_parse}, else: %PhoneNumber{}
+            phone_number = Map.merge(phone_number,
+              if extension = maybe_strip_extension(national_number) do
+                %{extension: extension}
+              else
+                %{}
+              end)
+            phone_number
+          end
+        end
+    end
   end
 
   defp build_national_number_for_parsing(number_to_parse) do
@@ -37,5 +58,4 @@ defmodule ExPhoneNumber.PhoneNumberUtil do
   defp check_region_for_parsing(number_to_parse, default_region) do
     Metadata.valid_region_code?(default_region) or Regex.match?(Pattern.leading_plus_char_pattern, number_to_parse)
   end
-
 end
