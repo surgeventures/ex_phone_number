9d7a487e446f35c77cfce6b00870dfd8df04ad00
Aaron Tinio <aptinio@gmail.com>
2017-01-18 19:18:22 +0800

    Relax Elixir requirement (#14)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExPhoneNumber.Mixfile do
   def project do
     [app: :ex_phone_number,
      version: "0.0.1",
-     elixir: "~> 1.3.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
