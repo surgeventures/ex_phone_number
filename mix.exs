b9cb54a1ad508873ba7ea53f130d0f36578fb497
Mariusz Cieniewski <mariusz@cieniew.ski>
2021-06-07 15:48:05 +0200

    Fix

--- mix.exs
+++ mix.exs
@@ -10,14 +10,12 @@ defmodule ExPhoneNumber.Mixfile do
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.travis": :test],
       deps: deps(),
       package: package(),
       description: description(),
       name: "ExPhoneNumber",
-      source_url: "https://github.com/socialpaymentsbv/ex_phone_number",
-      homepage_url: "https://github.com/socialpaymentsbv/ex_phone_number"
+      source_url: "https://github.com/surgeventures/ex_phone_number",
+      homepage_url: "https://github.com/surgeventures/ex_phone_number"
     ]
   end
 
@@ -30,23 +28,24 @@ defmodule ExPhoneNumber.Mixfile do
       {:sweet_xml, "~> 0.6.5"},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
       {:ex_spec, "~> 2.0", only: :test},
-      {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
-  defp description do """
-  A library for parsing, formatting, and validating international phone numbers. Based on Google's libphonenumber.
-  """
+  defp description do
+    """
+    A library for parsing, formatting, and validating international phone numbers. Based on Google's libphonenumber.
+    """
   end
 
   defp package do
     [
       files: ["lib", "config", "resources", "LICENSE*", "README*", "mix.exs"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/socialpaymentsbv/ex_phone_number"},
+      links: %{"GitHub" => "https://github.com/surgeventures/ex_phone_number"},
       maintainers: ["ClubCollect (@socialpaymentsbv)", "Jose Miguel Rivero Bruno (@josemrb)"],
-      name: :ex_phone_number
+      name: :ex_phone_number,
+      organization: "fresha"
     ]
   end
 end
