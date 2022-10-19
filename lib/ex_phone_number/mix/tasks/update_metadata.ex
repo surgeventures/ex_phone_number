fa14023f5d91755cb992a1ae01fd6506b5892d60
dkuku <daniel.kuku@gmail.com>
2022-10-19 09:19:24 +0100

    credo

--- lib/ex_phone_number/mix/tasks/update_metadata.ex
+++ lib/ex_phone_number/mix/tasks/update_metadata.ex
@@ -9,15 +9,15 @@ defmodule Mix.Tasks.UpdateMetadata do
 
   @impl Mix.Task
   def run(_args) do
-    Enum.map(@files_to_download, &download/1)
+    Enum.each(@files_to_download, &download/1)
     IO.puts("files updated, check any changes with git diff")
   end
 
   def download(filename) do
     local_path = Path.join([File.cwd!(), @resources_directory, filename])
 
-    with {:ok, {_status, _headers, body}} = :httpc.request([@remote_path <> filename]),
-         _ <- File.write(local_path, body) do
+    with {:ok, {_status, _headers, body}} <- :httpc.request([@remote_path <> filename]),
+         :ok <- File.write(local_path, body) do
       IO.puts("written #{length(body)} bytes to #{local_path}")
     end
   end
